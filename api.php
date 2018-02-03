c39569b3b671e7cf420b911fde567f3d101ca5db
Victor Durojaiye <durojaiyevic@gmail.com>
2018-02-03 13:23:57 +0100

    Update api.php

--- api.php
+++ api.php
@@ -14,70 +14,84 @@
     }
 
     else if (preg_match("/(1|2|3|4)\*/", $text)) {
-    	// query public APIs
-    	$data = explode("*", $text); 
-    	$l = count($data); 
+	    
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	    CURLOPT_RETURNTRANSFER => 1,
+	    CURLOPT_URL => "https://en.wikipedia.org/w/api.php?action=opensearch&search=hello%20world&limit=1&format=json",
+	    CURLOPT_USERAGENT => 'Victor'
+	));
+	$res = curl_exec($curl);
+	curl_close($curl);
 
-    	switch ($data[$l - 2]) {
-    		case "1":
-    			$curl = curl_init();
-    			curl_setopt_array($curl, array(
-				    CURLOPT_RETURNTRANSFER => 1,
-				    CURLOPT_URL => "https://en.wikipedia.org/w/api.php?action=opensearch&search=" . 
-				    str_replace(" ", "%20", $data[$l - 1]) . 
-				    "&limit=1&format=json",
-				    CURLOPT_USERAGENT => 'Victor'
-				));
-				$res = curl_exec($curl);
-				curl_close($curl);
+	$arr = json_decode($res, true); 
+	$response = "END " . (($arr[2][0] == "") ? "We were unable to find anything. Please try searching for something else." : $arr[2][0]); 
+    			
+	    
+//     	// query public APIs
+//     	$data = explode("*", $text); 
+//     	$l = count($data); 
 
-    			$arr = json_decode($res, true); 
-    			$response = "END " . (($arr[2][0] == "") ? "We were unable to find anything. Please try searching for something else." : $arr[2][0]); 
-    			break; 
+//     	switch ($data[$l - 2]) {
+//     		case "1":
+//     			$curl = curl_init();
+//     			curl_setopt_array($curl, array(
+// 				    CURLOPT_RETURNTRANSFER => 1,
+// 				    CURLOPT_URL => "https://en.wikipedia.org/w/api.php?action=opensearch&search=" . 
+// 				    str_replace(" ", "%20", $data[$l - 1]) . 
+// 				    "&limit=1&format=json",
+// 				    CURLOPT_USERAGENT => 'Victor'
+// 				));
+// 				$res = curl_exec($curl);
+// 				curl_close($curl);
 
-    		case "2":
-    		    $curl = curl_init();
-    			curl_setopt_array($curl, array(
-				    CURLOPT_RETURNTRANSFER => 1,
-				    CURLOPT_URL => "https://api.duckduckgo.com/?q=" . 
-				    str_replace(" ", "+", $data[$l - 1]) . 
-				    "&format=json&pretty=1",
-				    CURLOPT_USERAGENT => 'Victor'
-				));
-				$res = curl_exec($curl);
-				curl_close($curl);
+//     			$arr = json_decode($res, true); 
+//     			$response = "END " . (($arr[2][0] == "") ? "We were unable to find anything. Please try searching for something else." : $arr[2][0]); 
+//     			break; 
 
-    			$arr = json_decode($res, true); 
+//     		case "2":
+//     		    $curl = curl_init();
+//     			curl_setopt_array($curl, array(
+// 				    CURLOPT_RETURNTRANSFER => 1,
+// 				    CURLOPT_URL => "https://api.duckduckgo.com/?q=" . 
+// 				    str_replace(" ", "+", $data[$l - 1]) . 
+// 				    "&format=json&pretty=1",
+// 				    CURLOPT_USERAGENT => 'Victor'
+// 				));
+// 				$res = curl_exec($curl);
+// 				curl_close($curl);
 
-    			if ($arr["AbstractText"] != "") $response = "END " . $arr["AbstractText"]; 
-    			elseif ($arr["RelatedTopics"][0]["Text"] != "") $response = "END " . $arr["RelatedTopics"][0]["Text"]; 
-    			else $response = "END We were unable to find anything. Please try searching for something else."; 
-    			break; 
+//     			$arr = json_decode($res, true); 
 
-    		case "3":
-    			$curl = curl_init();
-    			curl_setopt_array($curl, array(
-				    CURLOPT_RETURNTRANSFER => 1,
-				    CURLOPT_URL => "http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=" . 
-				    str_replace(" ", "+", $data[$l - 1]),
-				    CURLOPT_USERAGENT => 'Victor'
-				));
-				$res = curl_exec($curl);
-				curl_close($curl);
+//     			if ($arr["AbstractText"] != "") $response = "END " . $arr["AbstractText"]; 
+//     			elseif ($arr["RelatedTopics"][0]["Text"] != "") $response = "END " . $arr["RelatedTopics"][0]["Text"]; 
+//     			else $response = "END We were unable to find anything. Please try searching for something else."; 
+//     			break; 
 
-    			$arr = json_decode($res, true); 
+//     		case "3":
+//     			$curl = curl_init();
+//     			curl_setopt_array($curl, array(
+// 				    CURLOPT_RETURNTRANSFER => 1,
+// 				    CURLOPT_URL => "http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=" . 
+// 				    str_replace(" ", "+", $data[$l - 1]),
+// 				    CURLOPT_USERAGENT => 'Victor'
+// 				));
+// 				$res = curl_exec($curl);
+// 				curl_close($curl);
 
-    			$len = $arr["count"]; 
-    			$response = "END "; 
+//     			$arr = json_decode($res, true); 
 
-    			if ($len == 0) $response .= "We were unable to find anything. Please try searching for something else."; 
+//     			$len = $arr["count"]; 
+//     			$response = "END "; 
 
-    			else {
-    				for ($i = 0; $i < $len; $i++)
-    					$response .= ($i + 1) . ". " . $arr["results"][$i]["senses"][0]["definition"][0] . "\n"; 
-    			}
-    			break; 
-    	}
+//     			if ($len == 0) $response .= "We were unable to find anything. Please try searching for something else."; 
+
+//     			else {
+//     				for ($i = 0; $i < $len; $i++)
+//     					$response .= ($i + 1) . ". " . $arr["results"][$i]["senses"][0]["definition"][0] . "\n"; 
+//     			}
+//     			break; 
+//     	}
     }
 
     else if (substr($text, -1) == "1" || substr($text, -1) == "2" || substr($text, -1) == "3" || substr($text, -1) == "4") {
